0f355dd5b1df9ca2a49483fca51b5e32b0db0575
AadityaBodke <aaditya.bodke0601@gmail.com>
2024-12-11 19:22:34 -0600

    updated readme

--- reflexion/webshop_runs/run_reflexion.sh
+++ reflexion/webshop_runs/run_reflexion.sh
@@ -1,5 +1,5 @@
 python main.py \
-        --num_trials 2 \
-        --num_envs 100 \
-        --run_name "reflexion_run_logs_3" \
+        --num_trials 3 \
+        --num_envs 10 \
+        --run_name "reflexion_run_logs_gpt4omini_test" \
         --use_memory
